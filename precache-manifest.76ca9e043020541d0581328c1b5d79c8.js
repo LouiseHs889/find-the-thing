3bc4c825323e3f24ed67130f85a7a091eed9b9cc
LouiseHs889 <Minapkb128@gmail.com>
2024-03-04 17:35:23 +0800

    Updates

---
+++ precache-manifest.76ca9e043020541d0581328c1b5d79c8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8494406deea3e9fb44d85a093d0d2272",
+    "url": "/find-the-thing/index.html"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/find-the-thing/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "776ff1a7904e7819f049",
+    "url": "/find-the-thing/static/js/main.2303b16c.chunk.js"
+  },
+  {
+    "revision": "b7bf0afa89e3865f05b6",
+    "url": "/find-the-thing/static/js/runtime~main.4036d7f8.js"
+  }
+]);
---
+++ static/js/main.2303b16c.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{12:function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),o=t(2);var c=function(){return r.a.createElement("div",null,r.a.createElement("h1",null,"I'm app !!"))};t.n(o).a.createRoot(document.getElementById("root")).render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(c,null)))},3:function(e,n,t){e.exports=t(12)}},[[3,1,2]]]);
+//# sourceMappingURL=main.2303b16c.chunk.js.map
